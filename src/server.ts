49b685c6f1600d710851d2c2f45a481d42c90f2e
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-09 17:51:49 -0300

    feat: add environment variables config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from './app';
+import { env } from './env';
 
 app.listen({
     host: "0.0.0.0",
-    port: 3333
+    port: env.PORT
 }).then(() => {
-    console.log(' 🚀 server is running at http://localhost:3333')
+    console.log(`🚀 server is running at http://localhost:${env.PORT}`)
 })
