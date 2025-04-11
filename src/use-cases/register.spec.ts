19003ce1737d1c1cf6ebe8b9b665aa8dffa79fb9
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-11 20:57:56 -0300

    Feat: add register unit tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,11 +1,30 @@
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
 import { RegisterUseCase } from './register'
 
 describe('Register use cases tests', () => {
+  it('should to be able to register a new user', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Jonny Test',
+      email: 'jonnytest@gmail.com',
+      password: 'jtest1234',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(usersRepository.items.length).toBe(1)
+    expect(usersRepository.items[0]).contain({
+      name: 'Jonny Test',
+      email: 'jonnytest@gmail.com',
+    })
+  })
+
   it('should hash user password upon user registration', async () => {
-    const usersRepository = new PrismaUsersRepository()
+    const usersRepository = new InMemoryUsersRepository()
     const registerUseCase = new RegisterUseCase(usersRepository)
 
     const { user } = await registerUseCase.execute({
@@ -20,5 +39,24 @@ describe('Register use cases tests', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not to be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name: 'Jonny Test',
+      email: 'jonnytest@gmail.com',
+      password: 'jtest1234',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'Jonny Test',
+        email: 'jonnytest@gmail.com',
+        password: 'jtest1234',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
