b7971fa7cb2854c4965bdf6dc0658f63e81328d2
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-13 17:50:49 -0300

    feat: add get user profile use case

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,29 @@
+import type { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import type { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseReply {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: PrismaUsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseReply> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
