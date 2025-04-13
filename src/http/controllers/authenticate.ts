ed30e09578da9bea5855145f7276118a4e05811a
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-13 00:01:32 -0300

    feat: add factory pattern to structure use cases

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -15,10 +14,9 @@ export async function authenticate(
 
   const { email, password } = authenticateUserBodySchema.parse(request.body)
 
-  const usersRepository = new PrismaUsersRepository()
-  const authenticateUseCase = new AuthenticateUseCase(usersRepository)
-
   try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+
     await authenticateUseCase.execute({
       email,
       password,
