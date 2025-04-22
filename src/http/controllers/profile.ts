3801b4eb6df13a704958bef5e376073b3dd67b4b
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-21 21:59:54 -0300

    feat: add authentication flow

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,7 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify()
+
+  return reply.status(200).send()
+}
