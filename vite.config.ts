27611c01d0d42b396e83f212a1587d20a0bb29f8
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-04-28 22:31:03 -0300

    test(e2e): setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,25 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/constrollers',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
+  },
 })
